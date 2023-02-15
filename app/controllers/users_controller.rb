93d18af0634786d565a1aa4fe77b6a5c68df03be
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-02-15 18:03:02 +0530

    'Create_helpermethods'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
   
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5 )
   end
 
@@ -14,11 +14,10 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
   end
 
   def update 
-    @user = User.find(params[:id]) 
+   
     if @user.update(user_params) 
       flash[:notice] = "Welcome To The Blog"
       redirect_to articles_path
@@ -31,6 +30,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save 
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome To The Blog"
       redirect_to articles_path
     else
@@ -39,6 +39,10 @@ class UsersController < ApplicationController
   end
   
   private
+
+  def set_user
+    @user = User.find(params[:id]) 
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
