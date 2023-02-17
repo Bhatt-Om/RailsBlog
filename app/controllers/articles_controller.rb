49e1169ee4cca3c7ac7222d35f37c515fd772e40
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-02-17 17:15:21 +0530

    Give_Admin_To_Privilege_Edit_Or_Delete_Users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:notice] = "You can only Edit Or Delete Your Own Article"
       redirect_to @article
     end
